21ac4f9f0324f1f37c24113ea78fd7c68c210b43
DSoftheM <braveartur@mail.ru>
2022-11-15 15:39:32 +0500

    blur on open modal

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,13 @@ import Header from "./components/Header/Header";
 import './App.scss';
 import Content from "./components/Content/Content";
 import Modal from "./components/Modal/Modal";
+import { useAppSelector } from "./redux/hooks/hooks";
 
 function App() {
+	const isModalOpen = useAppSelector(({ modalReducer: { isOpen } }) => isOpen);
+
 	return (
-		<div className="app">
+		<div className={`app ${isModalOpen ? 'blured' : ''}`}>
 			<Header />
 			<Content />
 			<Modal />
